8ed7218142bfda2d99faeeb63dd2e5614a882f32
DavidHolmes0 <David@DavidMHolmes.net>
2013-07-29 15:39:49 -0400

    establish standard test files in the repository

--- cgi-bin/ngButOKToApache.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-
-import cgitb; cgitb.enable()
-print ':\n\n'\
-      '<head></head> <body>'
-print "No content type, but Apache displays this "\
-      "as though content were of type plain text 16"
-print '</body></head>'
