f90e6d5963b3bc69a91c221e76f2207b7cbf5658
DavidHolmes0 <David@DavidMHolmes.net>
2013-08-06 13:59:18 -0400

    revise tests to eliminate cgi-bin, html-stuff

--- tests/test_html.py
+++ tests/test_html.py
@@ -2,7 +2,7 @@ HTML_HEADER='Content-type: text/html\n\n'
 
 def Main():
     print HTML_HEADER+'<html><body>'
-    print '<b> Success. Python is able to generate HTML text from the <i>cgi-bin/python-stuff</i> directory.</b><p>'
+    print '<b> Success:  The server can have Python generate HTML.<br>'
     print '(Now hit the BACK button)</body></html>'
 
 Main()
